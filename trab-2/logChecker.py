4187afb068062fd9658a06995a07e9ccaf86ddab
Nidnogg <henriquevt98@gmail.com>
2019-11-29 07:25:42 -0200

    Python log checker protoypes added

---
+++ trab-2/logChecker.py
@@ -0,0 +1,19 @@
+
+def tRead(tid, readValue):
+
+def tReaderBlocked(tid, writing, waitingToWrite, writeTurn):
+	"""Diz que leitor foi bloqueado, por conta dos parâmetros"""
+
+def tReaderUnblocked(tid, writing):
+
+def ReaderSignalled(tid, reading):
+
+def tWrote(tid, readValue):
+
+def tWriterBlocked(tid, reading, writing, waitingToRead, writeTurn):
+	"""Diz que leitor foi bloqueado, por causa dos parâmetros"""
+
+def tWriterUnblocked(tid, reading, writing, waitingToRead, writeTurn):
+
+def WriterSignalledBroadcasted(tid):
+
